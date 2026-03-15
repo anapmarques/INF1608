d5b48ddc95578d0b5a747c5c05a87dc06fc002bf
Ana Luiza <aninhapmarques03@gmail.com>
2026-03-15 19:28:13 -0300

    final lab1

--- lab1/taylor.c
+++ lab1/taylor.c
@@ -2,11 +2,35 @@
 #include <stdlib.h>
 
 double avalia_taylor (int n, double* c, double x0, double x) {
-    double p = 0;
+    double p = 0.0;
+    double fat = 1.0;
+    double exp = 1.0;
 
     for (int i = 0; i < n; i++) {
-        p += (c[i] / i * (i - 1)) * ((x - x0) ^ i);
+        if (i != 0)  {
+            fat *= i;
+            exp *= (x - x0);
+        }
+        p += (c[i] / fat) * (exp);
     }
 
     return p;
+}
+
+double avalia_seno (int n, double x) {
+    double c[n];
+    for (int i = 0; i < n; i++){
+        if (i % 2 == 0){
+            c[i] = 0;
+        }
+        else{
+            if (((i - 1) / 2) % 2 == 0){
+                c[i] = 1;
+            }
+            else {
+                c[i] = -1;
+            }
+        }
+    }
+    return avalia_taylor(n, c, 0.0, x);
 }
