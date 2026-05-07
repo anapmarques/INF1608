9f5be46b95c8a7a134f5d4b05b52cda0cfacc67c
Ana Luiza Marques <113355947+anapmarques@users.noreply.github.com>
2026-05-07 18:36:42 -0300

    ajustando lab6

--- lab6/derivadaintegral.c
+++ lab6/derivadaintegral.c
@@ -6,33 +6,32 @@ double derivada (double (*f) (double), double x, double h) {
 }
 
 double simpson (double (*f) (double), double a, double b, int n) {
+
     double h = (b - a) / n;
-    double soma = f(a) + f(b);
-
-    for (int i = 1; i < n; i++) {
-        double x = a + i * h;
-
-        if (i % 2 == 0) {
-            soma += 2 * f(x);
-        } else {
-            soma += 4 * f(x);
-        }
+    double soma = 0.0;
+    double x, xm;
+    
+    for (int i = 0; i < n; i++) {
+        x = a + i * h;
+        xm = x + h / 2.0;
+        
+        soma += (h / 6.0) * (f(x) + (4.0 * f(xm)) + f(x + h));
     }
-
-    return (h / 3.0) * soma;
+    
+    return soma;
 }
 
 double simpsonadaptativo (double (*f) (double), double a, double b, double tol) {
-    double c = (a + b) / 2;
+    double c = (a + b) / 2.0;
     
-    double Sab = simpson(f, a, b, 2);
-    double Sac = simpson(f, a, c, 2);
-    double Scb = simpson(f, c, b, 2);
+    double Sab = simpson(f, a, b, 1);
+    double Sac = simpson(f, a, c, 1);
+    double Scb = simpson(f, c, b, 1);
     
-    double delta = Sab - Sac - Scb;
+    double delta = Sab - (Sac + Scb);
     
-    if (fabs(delta) < (15*tol)) {
-        return Sac + Scb - (delta/15);
+    if (fabs(delta) < (15.0*tol)) {
+        return Sac + Scb - (delta/15.0);
     }
-    else return simpsonadaptativo(f, a, c, tol/2) + simpsonadaptativo(f, c, b, tol/2);
+    else return simpsonadaptativo(f, a, c, tol/2.0) + simpsonadaptativo(f, c, b, tol/2.0);
 }
